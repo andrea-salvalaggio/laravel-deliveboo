634ec29eb086dadae924e42646ca586747e81466
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-10-11 13:30:17 +0200

    feat: :sparkles: Orders Table Update
    Add name, surname, address, comment, price column

--- database/migrations/2022_10_11_110044_create_orders_table.php
+++ database/migrations/2022_10_11_110044_create_orders_table.php
@@ -15,6 +15,11 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->text('address');
+            $table->text('comment')->nullable();
+            $table->float('price')->unsigned();
             $table->timestamps();
         });
     }
