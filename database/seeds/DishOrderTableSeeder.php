f9b392976faff616387b2ef7754d6f3de15b09b4
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-10-12 16:16:30 +0200

    feat: :bug: Fix Bug In DishOrderTableSeeder

--- database/seeds/DishOrderTableSeeder.php
+++ database/seeds/DishOrderTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\Dish;
 use App\Models\Order;
+use App\Models\Restaurant;
 use Illuminate\Database\Seeder;
 
 class DishOrderTableSeeder extends Seeder
@@ -17,7 +18,18 @@ class DishOrderTableSeeder extends Seeder
         $dishes = Dish::all();
 
         foreach ($orders as $order) {
-            $randomDishes = Dish::inRandomOrder()->limit(rand(1, 3))->get();
+            // $dishes = $order->restaurant;
+            $randomDishes=[];
+            foreach($dishes as $dish) {
+                if ($dish->restaurant_id == $order->restaurant_id) {
+                    $randomDishes[] = $dish;
+                }
+            }
+
+            // $randomDishes=[];
+            // for ($i=0; $i < rand(1,4); $i++) { 
+            //     $randomDishes[$i] = $dishes[0];
+            // }
             
             foreach ($randomDishes as $dish) {
                 $order->dishes()->attach($dish, ['quantity'=>rand(1,3)]);
