cc915e1e94375627830c596688388bd92c240494
vtnboolean <vnthuanx@gmail.com>
2022-10-14 16:23:35 +0200

    feat: :sparkles: text-centered categories cards

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -5,7 +5,7 @@
 <div class="container-lg mt-5">
     <div class="row">
 @forelse ($categories as $category)
-        <div class="col-4 my-4">
+        <div class="col-4 my-4 text-center">
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
                   <h3 class="card-title">{{ $category->name }}</h3>
