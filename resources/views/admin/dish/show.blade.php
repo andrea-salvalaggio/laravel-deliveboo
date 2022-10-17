618f7dbc3c0d06bdc61f19cdfad9c31507b49925
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-17 20:09:35 +0200

    style: :art: Add box shadow to card container

--- resources/views/admin/dish/show.blade.php
+++ resources/views/admin/dish/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-lg mt-4">
-        <div class="card my-rounded mx-auto w-50 border-0">
+        <div class="card my-rounded my-shadow mx-auto w-50 border-0">
             {{-- @dd(asset('/' . $dish->dishPic)) --}}
             @if (filter_var($dish->dishPic, FILTER_VALIDATE_URL))
                 <img src="{{ $dish->dishPic }}" class="card-img-top my-rounded-top" alt="{{ $dish->name }} photo">
@@ -15,9 +15,9 @@
                 <img src="{{ asset('storage/' . $dish->dishPic)}}" class="card-img-top" alt="{{ $dish->name }} photo">
             @endif --}}
             <div class="card-body p-5">
-                <h5 class="card-title text-capitalize">{{ $dish->name }}</h5>
+                <h5 class="card-title text-capitalize font-weight-bold h2">{{ $dish->name }}</h5>
                 <p class="card-text">{{ $dish->description }}</p>
-                <p class="card-text">Price: &nbsp; {{ $dish->price }} €</p>
+                <p class="card-text h4 font-weight-lighter">{{ $dish->price }} €</p>
                 <div class="d-flex mt-5">
                     <div class="col-6 p-0 pr-2">
                         <a href="{{ route('admin.dish.edit', $dish->id) }}" class="btn my-btn-success w-100 rounded-pill text-white my-btn-shadow p-2">Edit</a>
