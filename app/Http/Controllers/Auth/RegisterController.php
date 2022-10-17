132e7798316b94a9e6956f912e79d54229108aca
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-17 13:10:04 +0200

    fix: :bug: Validation User for registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -57,10 +57,10 @@ class RegisterController extends Controller
         // ]);
 
         return Validator::make($data, [
-            'name' => ['required', 'string', 'min:2', 'max:30', 'unique:users'],
+            'name' => ['required', 'string', 'min:2', 'max:30'],
             'email' => ['required', 'string', 'email', 'min:5', 'max:40', 'unique:users'],
-            'p_iva' => ['required', 'min:11', 'max:11', 'numeric'],
-            'password' => ['required', 'string', 'min:8', 'max:20', 'confirmed'],
+            'p_iva' => ['required', 'size:11'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
 
