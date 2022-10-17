64e06f2dd374ae09a3a7114be892b24b80343412
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-17 20:02:56 +0200

    style: :art: Add border radius to card container

--- resources/views/admin/dish/show.blade.php
+++ resources/views/admin/dish/show.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <div class="container-lg mt-4">
-        <div class="card mx-auto w-50 border-0">
+        <div class="card my-rounded mx-auto w-50 border-0">
             {{-- @dd(asset('/' . $dish->dishPic)) --}}
             @if (filter_var($dish->dishPic, FILTER_VALIDATE_URL))
-                <img src="{{ $dish->dishPic }}" class="card-img-top" alt="{{ $dish->name }} photo">
+                <img src="{{ $dish->dishPic }}" class="card-img-top my-rounded-top" alt="{{ $dish->name }} photo">
             @else
-                <img src="{{ asset('storage/' . $dish->dishPic)}}" class="card-img-top" alt="{{ $dish->name }} photo">
+                <img src="{{ asset('storage/' . $dish->dishPic)}}" class="card-img-top my-rounded-top" alt="{{ $dish->name }} photo">
             @endif
             {{-- @if (substr(filter_var($dish->dishPic), 0, 4) === 'http')
                 <img src="{{ $dish->dishPic }}" class="card-img-top" alt="{{ $dish->name }} photo">
@@ -20,13 +20,13 @@
                 <p class="card-text">Price: &nbsp; {{ $dish->price }} €</p>
                 <div class="d-flex mt-5">
                     <div class="col-6 p-0 pr-2">
-                        <a href="{{ route('admin.dish.edit', $dish->id) }}" class="btn btn-success w-100 rounded-pill my-btn-shadow p-2">Edit</a>
+                        <a href="{{ route('admin.dish.edit', $dish->id) }}" class="btn my-btn-success w-100 rounded-pill text-white my-btn-shadow p-2">Edit</a>
                     </div>
                     <div class="col-6 p-0 pl-2">
                         <form action="{{ route('admin.dish.destroy', $dish->id) }}" method="POST" class="btn-delete">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-danger w-100 rounded-pill my-btn-shadow p-2">Delete</button>
+                            <button type="submit" class="btn my-btn-danger w-100 rounded-pill text-white my-btn-shadow p-2">Delete</button>
                         </form>
                     </div>
                 </div>
