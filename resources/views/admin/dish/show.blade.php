c4111e1e1b8d6ec45b833e3dbfb2a78af5b0e4a5
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-18 10:56:59 +0200

    style: :art: Fix button size on responsive layout

--- resources/views/admin/dish/show.blade.php
+++ resources/views/admin/dish/show.blade.php
@@ -18,12 +18,12 @@
                 <h5 class="card-title text-capitalize font-weight-bold h2">{{ $dish->name }}</h5>
                 <p class="card-text my-4">{{ $dish->description }}</p>
                 <hr>
-                <p class="card-text h4 font-weight-lighter mt-4">€ {{ $dish->price }}</p>
-                <div class="d-flex mt-5">
-                    <div class="col-6 p-0 pr-2">
+                <p class="card-text h4 font-weight-lighter mt-4"><span class="euro-price">€</span> {{ $dish->price }}</p>
+                <div class="row mt-5">
+                    <div class="col-12 p-0">
                         <a href="{{ route('admin.dish.edit', $dish->id) }}" class="btn my-btn-success w-100 rounded-pill text-white my-btn-shadow p-2">Edit</a>
                     </div>
-                    <div class="col-6 p-0 pl-2">
+                    <div class="col-12 p-0 mt-3">
                         <form action="{{ route('admin.dish.destroy', $dish->id) }}" method="POST" class="btn-delete">
                             @csrf
                             @method('DELETE')
