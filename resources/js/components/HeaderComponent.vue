d80be71ab2eed951ad5f2b5e28134db448680ba4
stecala <ste.cala123@gmail.com>
2022-10-18 10:39:00 +0200

    feat: :sparkles: created header component and cleaned app.vue

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <div class="container-lg">
+            <div class="row justify-content-between">
+                <div class="col-4"></div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+
+</script>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <section>
-    <div class="container-lg text-center mt-5">
+  <div>
+    <HeaderComponent />
+    <main class="container-lg text-center mt-5">
 
-        <a href="/login" class="btn btn-primary rounded-pill">Login</a>
-        <a href="/register" class="btn btn-warning rounded-pill">Register</a>
-    </div>
-    <div class="container-lg containerdaeliminare">
-      <img src="https://sicilia.agesci.it/nautici/wp-content/uploads/sites/12/2018/05/wip.png" alt="working in progress">
-    </div>
-  </section>
+       <!--  <a href="/login" class="btn btn-primary rounded-pill">Login</a>
+        <a href="/register" class="btn btn-warning rounded-pill">Register</a> -->
+    </main>
+  </div>
 </template>
 
 <script>
+import HeaderComponent from '../components/HeaderComponent.vue'
 export default {
-
+  components:{
+    HeaderComponent,
+  }
 }
 </script>
 
