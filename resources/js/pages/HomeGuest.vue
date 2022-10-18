566f7687b3f73c4d9c17e2a2aa9ce6fc954a6da0
stecala <ste.cala123@gmail.com>
2022-10-18 19:14:46 +0200

    feat: :sparkles: now the toggle works

--- resources/js/pages/HomeGuest.vue
+++ resources/js/pages/HomeGuest.vue
@@ -4,8 +4,13 @@
 
     <!-- categorie -->
     <div class="container-lg">
-      <div class="row">
-        <CategoryComponent v-for="category in categories" :key="category.id" :category="category" />
+      <div class="row"> 
+        <div class="col-3 my-4" v-for="category in categories" :key="category.id">
+          <div class="mx-auto  my-rounded my-shadow order-card d-flex align-items-center justify-content-center text-capitalize"  :id="'categoria'+category.id"
+              @click="activeCard(category)" >
+              {{ category.name }}
+          </div>
+         </div>
       </div>
     </div>
 
@@ -14,14 +19,16 @@
 
 <script>
 import axios from 'axios';
-import CategoryComponent from '../components/CategoryComponent.vue';
 export default {
   components:{
-    CategoryComponent,
   },
   data: function(){
     return{
       categories: [],
+      currentActive: '',
+      isClicked: false,
+      idCategory: null,
+      activeClass: '',
     }
   },
   methods:{
@@ -35,6 +42,16 @@ export default {
       .catch((error)=>{
         console.error(error)
       })
+    },
+    activeCard(category){
+      if( document.querySelector('.active-card') != null ){
+        document.querySelector('.active-card').classList.remove('active-card')
+      }
+      this.idCategory = category.id
+      this.currentActive= document.getElementById('categoria'+this.idCategory).classList.add('active-card')
+
+      console.log(this.idCategory)
+      /* if(this.currentActive == this.idCategory &&) */
     }
   },
   created(){
@@ -43,6 +60,19 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+ @import '../../sass/variables';
 
+    .order-card{
+        width: 90%;
+        height: 100px;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+        color: $secondaryColor;
+        cursor: pointer;
+    }
+    .active-card{
+        background-color: $primaryColor;
+        color: white;
+    } 
 </style>
