27271e67faada06131f910b6e75c387b47991fa7
stecala <ste.cala123@gmail.com>
2022-10-18 16:20:16 +0200

    feat: :sparkles: created function for api call category

--- resources/js/pages/HomeGuest.vue
+++ resources/js/pages/HomeGuest.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
-    
+    <!-- banner -->
+
+    <!-- categorie -->
+    <div class="container-lg">
+      <div class="row">
+        <CategoryComponent v-for="category in categories" :key="category.id" :category="category" />
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import CategoryComponent from '../components/CategoryComponent.vue';
 export default {
-
+  components:{
+    CategoryComponent,
+  },
+  data: function(){
+    return{
+      categories: [],
+    }
+  },
+  methods:{
+    //! funzione per effettuare la chiamata 
+    ApiCallAllCategories(){
+      axios.get('/api/category')
+      .then((result)=>{
+        this.categories= result.data.results
+        console.log(this.categories)
+      })
+      .catch((error)=>{
+        console.error(error)
+      })
+    }
+  },
+  created(){
+    this.ApiCallAllCategories()
+  }
 }
 </script>
 
