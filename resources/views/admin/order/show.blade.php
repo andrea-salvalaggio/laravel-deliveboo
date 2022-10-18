ff1d2fc2b850acd7bdb73b5d6c46d19d429a1b50
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-18 11:12:12 +0200

    style: :art: Add style to Delete button

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
     <div class="container-lg mt-4">
-        <div class="card mx-auto w-50 border-0" style="width: 18rem;">
+        <div class="card mx-auto w-50 border-0">
             <div class="card-body">
                 <h5 class="card-title text-capitalize">{{ $order->name . ' ' . $order->surname}}</h5>
                 <p class="card-text">{{ $order->address }}.</p>
                 <p class="card-text">{{ $order->description }}.</p>
                 <p class="card-text">Price: {{ $order->price }} €</p>
-                <div class="d-flex">
-                    <div class="mx-2">
+                <div class="row">
+                    <div class="col-12 p-0">
                         <form action="{{ route('admin.order.destroy', $order->id) }}" method="POST" class="btn-delete">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-danger rounded-pill">Delete</button>
+                            <button type="submit" class="btn my-btn-danger w-100 rounded-pill text-white my-btn-shadow">Delete</button>
                         </form>
                     </div>
                 </div>
