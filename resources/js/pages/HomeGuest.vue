0b60c16c86919426bae757f3bbae9045a81ee18e
Vegadrone <spenaantonino@gmail.com>
2022-10-18 15:34:06 +0100

    feat: :sparkles: add footer and link footer to home guest

--- resources/js/pages/HomeGuest.vue
+++ resources/js/pages/HomeGuest.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    
+    <FooterComponent />
   </div>
 </template>
 
 <script>
+import FooterComponent from '../components/FooterComponent.vue';
 export default {
-
-}
+    components: {
+        FooterComponent,
+    },
+};
 </script>
 
 <style>
