f8aa0890589b24e7791aef315e366033a1b82701
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-18 15:54:12 +0200

    style: :art: Add style to user button when logged

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,8 +66,9 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <li class="nav-item dropdown btn user-pill rounded-pill px-3">
+                                
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle p-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
