a41900d1fb5267bc4e35f943e294acdbdf5e3112
Vegadrone <spenaantonino@gmail.com>
2022-10-18 16:15:07 +0100

    style: :lipstick: aesthetics improvements in footer

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -9,79 +9,92 @@
     <div class="row">
 
       <!-- Grid column -->
-      <div class="col-md-6 mt-md-0 mt-3">
+      <div class="col-md-3 mt-md-0 mt-3">
 
         <!-- Content -->
-        <h5 class="text-uppercase">Footer Content</h5>
-        <p>Here you can use rows and columns to organize your footer content.</p>
+        <h5>qui ci va il logo monocromatico</h5>
 
       </div>
       <!-- Grid column -->
 
       <hr class="clearfix w-100 d-md-none pb-3">
 
-      <!-- Grid column -->
+        <!-- Grid column -->
       <div class="col-md-3 mb-md-0 mb-3">
 
         <!-- Links -->
-        <h5 class="text-uppercase">Links</h5>
+        <h5 class="text-uppercase">Join Us</h5>
 
         <ul class="list-unstyled">
           <li>
-            <a href="#!">Link 1</a>
+            <a href="#!">Careers</a>
           </li>
           <li>
-            <a href="#!">Link 2</a>
+            <a href="#!">Restaurants</a>
           </li>
           <li>
-            <a href="#!">Link 3</a>
+            <a href="#!">Become a Courier</a>
           </li>
           <li>
-            <a href="#!">Link 4</a>
+            <a href="#!">About</a>
           </li>
         </ul>
 
       </div>
-      <!-- Grid column -->
 
       <!-- Grid column -->
       <div class="col-md-3 mb-md-0 mb-3">
 
         <!-- Links -->
-        <h5 class="text-uppercase">Links</h5>
+        <h5 class="text-uppercase">Let us help you</h5>
 
         <ul class="list-unstyled">
           <li>
-            <a href="#!">Link 1</a>
+            <a href="#!">Help Center</a>
           </li>
           <li>
-            <a href="#!">Link 2</a>
+            <a href="#!">Support</a>
           </li>
           <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
+            <a href="#!">Contacts</a>
           </li>
         </ul>
 
       </div>
-      <!-- Grid column -->
 
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links 3 -->
+        <h5 class="text-uppercase">Follow us</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Facebook</a>
+          </li>
+          <li>
+            <a href="#!">Twitter</a>
+          </li>
+          <li>
+            <a href="#!">Instagram</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <!-- Grid row -->
-
+     <div class="col-12 divisor">
   </div>
-  <!-- Footer Links -->
+  </div>
+
+
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="/"> MDBootstrap.com</a>
+  <div class="footer-copyright text-center py-3">
+        All right reserved Made with CUORICINO by Team 4 - Class #67
+        <a href="/"></a>
   </div>
-  <!-- Copyright -->
 
 </footer>
-<!-- Footer -->
+
 </template>
 
 <script>
@@ -90,8 +103,24 @@ export default {
 }
 </script>
 
-<style lang 'sass' scoped>
+<style lang='scss' scoped>
 
-@import '../../sass/varables';
+@import '../../sass/variables';
+
+    footer{
+        background-color: $footerColor;
+    }
+
+    .divisor{
+        border-bottom: 1px solid white,
+    }
+
+    li,
+    p,
+    h5{
+        color: white;
+    }
+
+
 
 </style>
