a205ad5386aaa57bec2bee125acccebb906354f9
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-18 11:31:44 +0200

    style: :art: Finish style to card in Orders

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -4,14 +4,14 @@
     <div class="container-lg mt-4">
         <div class="card col-lg-6 col-md-6 col-sm-10 mx-auto my-rounded my-shadow mx-auto border-0">
             <div class="card-body p-5">
-                <h2 class="font-weight-bold">Details</h2>
-                <hr>
+                <h2 class="font-weight-bold">Details:</h2>
                 <p class="card-title text-capitalize mt-5">Name: &nbsp;{{ $order->name . ' ' . $order->surname}}</p>
                 <p class="card-text">Address: &nbsp;{{ $order->address }}</p>
                 <p class="card-text text-capitalize">Other info: &nbsp;{{ $order->description }}.</p>
-                <p class="card-text">Price: {{ $order->price }} €</p>
+                <hr class="my-4">
+                <p class="mb-0">Total Price: <span class="card-text font-weight-bolder h4 align-botton">&nbsp;€ {{ $order->price }} </span></p>
                 <div class="row">
-                    <div class="col-12 p-0">
+                    <div class="col-12 p-0 mt-5">
                         <form action="{{ route('admin.order.destroy', $order->id) }}" method="POST" class="btn-delete">
                             @csrf
                             @method('DELETE')
