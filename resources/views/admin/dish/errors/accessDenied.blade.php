b8cbc2af37bbd0e584a893083ed2d5560ea2d21a
vtnboolean <vnthuanx@gmail.com>
2022-10-19 18:05:29 +0200

    style: :sparkles: added background and image

--- resources/views/admin/dish/errors/accessDenied.blade.php
+++ resources/views/admin/dish/errors/accessDenied.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-lg text-center">
-        <img src="https://i.etsystatic.com/32485847/r/il/640f42/3631048046/il_340x270.3631048046_fbkq.jpg" alt="no pass">
+    <div class="row">
+        <div class="container-fluid myBackground text-center align-self-center">
+            <img class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-4  error-image pt-3" src="{{ asset('images/hamburger.png') }}" alt="no pass">
+        </div>
     </div>
+
+    <h1 class="text-center">Page Not Found</h1>
+    <div class="text-error text-center">Sorry, but the page you are looking for doesn’t exist.</div>
+    <div class="text-error text-center">But there’s loads more to see!</div>
+    <div class="nav-item my-btn rounded-pill py-1 px-4 my-btn-shadow mt-2 mt-md-0 text-center ">
+        <a class="nav-link text-white px-0" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+    </div>
+
 @endsection
