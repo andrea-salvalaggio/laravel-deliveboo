7c7a031091a2c5b68bd098dc4eea6330f4a442ed
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-19 16:13:31 +0200

    style: :art: Fix logo img dimension in desktop view

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,67 +1,75 @@
 <template>
   <footer class="p-5">
     <div class="container-lg">
-      <div class="row my-5">
+      <div class="row justify-content-between">
+
         <!-- Colonna logo -->
-        <div class="col-3 mt-5">
-          <!-- Logo AL MOMENTO PLACEHOLDER -->
+        <div class="col-3 img-container">
+
+          <!-- Logo -->
+          <img
+            src="https://cdn.discordapp.com/attachments/1027584701415833620/1032191240126611536/deliveboo-logo-invert-mobile.png"
+            alt="logo"
+            class="d-lg-none"
+          />
           <img
             src="https://media.discordapp.net/attachments/1027584701415833620/1032191239577153586/deliveboo-logo-invert-desktop.png"
             alt="logo"
+            class="d-none d-lg-block w-75"
           />
         </div>
 
         <!-- Colonna join us -->
-        <div class="col-3">
+        <div class="col-6 col-md-3 mb-5">
           <!-- Links -->
           <h5 class="text-uppercase font-weight-bold h4">join us</h5>
           <ul class="list-unstyled">
-            <li class="mt-3">
+            <li class="my-2">
               <a href="#!">Careers</a>
             </li>
-            <li class="mt-2">
+            <li class="my-2">
               <a href="#!">Restaurants</a>
             </li>
-            <li class="mt-2">
+            <li class="my-2">
               <a href="#!">Become a Courier</a>
             </li>
-            <li class="mt-2">
+            <li class="my-2">
               <a href="#!">About</a>
             </li>
           </ul>
         </div>
 
         <!-- Colonna Let us help you -->
-        <div class="col-3">
+        <div class="col-6 col-md-3 mb-5">
           <!-- Links -->
           <h5 class="text-uppercase font-weight-bold h4">let us help you</h5>
 
           <ul class="list-unstyled">
-            <li class="mt-3">
+            <li class="my-2">
               <a href="#!">Help Center</a>
             </li>
-            <li class="mt-2">
+            <li class="my-2">
               <a href="#!">Support</a>
             </li>
-            <li class="mt-2">
+            <li class="my-2">
               <a href="#!">Contacts</a>
             </li>
           </ul>
         </div>
 
         <!-- Colonna Follow us -->
-        <div class="col-3">
+        <div class="col-6 col-md-3 mb-5">
           <!-- Links 3 -->
           <h5 class="text-uppercase font-weight-bold h4">follow us</h5>
 
           <ul class="list-unstyled">
-            <li class="mt-3">
+            <li class="my-2">
               <a href="#!">Facebook</a>
             </li>
-            <li class="mt-2">
+            <li class="my-2">
               <a href="#!">Twitter</a>
             </li>
-            <li class="mt-2">
+            <li class="my-2">
               <a href="#!">Instagram</a>
             </li>
           </ul>
@@ -72,11 +80,11 @@
       <div class="col-12 divisor"></div>
 
       <!-- Copyright -->
-      <div class="copyright-container d-flex justify-content-between my-4">
-        <div class="footer-copyright mt-4">
+      <div class="copyright-container d-flex justify-content-between mt-4">
+        <div class="footer-copyright">
           <p>© All right reserved Made &#129505; by Team 4 - Class #67</p>
         </div>
-        <div class="footer-copyright-links mt-4">
+        <div class="footer-copyright-links">
           <p>
             <a class="mx-2" href="#!">Privacy Policy</a> |
             <a class="mx-2" href="#!">Terms & condition</a> |
@@ -97,6 +105,16 @@ export default {};
 
 footer {
   background-color: $footerColor;
+
+  .img-container {
+    img {
+      width: 100%;
+    }
+    .d-lg-none {
+      max-width: 72px;
+      min-width: 72px;
+    }
+  }
 }
 
 .divisor {
