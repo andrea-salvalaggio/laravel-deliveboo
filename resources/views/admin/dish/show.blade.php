22c7b9d203f46e575ed800ce9036f6740f1fc049
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-19 12:39:03 +0200

    fix: :sparkles: Fix visibile status on dish show

--- resources/views/admin/dish/show.blade.php
+++ resources/views/admin/dish/show.blade.php
@@ -4,6 +4,7 @@
     <div class="container-lg my-5">
         <div class="card col-lg-6 col-md-6 col-sm-10 my-rounded my-shadow mx-auto border-0">
             {{-- @dd(asset('/' . $dish->dishPic)) --}}
+
             @if (filter_var($dish->dishPic, FILTER_VALIDATE_URL))
                 <img src="{{ $dish->dishPic }}" class="card-img-top my-rounded-top" alt="{{ $dish->name }} photo">
             @else
@@ -14,7 +15,15 @@
             @else
                 <img src="{{ asset('storage/' . $dish->dishPic)}}" class="card-img-top" alt="{{ $dish->name }} photo">
             @endif --}}
+
             <div class="card-body p-5">
+
+                @if ($dish->visible==0)
+                    <div class="badge my-badge-success badge-pill w-25 mb-3"><small>available</small></div>
+                @else
+                    <div class="badge my-badge-danger badge-pill w-25 mb-3"><small>not available</small></div>
+                @endif
+                
                 <h2 class="card-title text-capitalize font-weight-bold">{{ $dish->name }}</h2>
                 <p class="card-text my-4">{{ $dish->description }}</p>
                 <hr>
