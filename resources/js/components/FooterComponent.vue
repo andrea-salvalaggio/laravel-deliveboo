1c932b618eb0f806097578739aabf99ee9c95d4a
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-19 10:21:43 +0200

    style: :art: Fix some margin

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,66 +1,67 @@
 <template>
   <footer class="p-5">
     <div class="container-lg">
-      <div class="row">
-
+      <div class="row my-5">
         <!-- Colonna logo -->
         <div class="col-3 mt-5">
-
           <!-- Logo AL MOMENTO PLACEHOLDER -->
-          <img src="https://media.discordapp.net/attachments/1027584701415833620/1032191239577153586/deliveboo-logo-invert-desktop.png" alt="logo" />
+          <img
+            src="https://media.discordapp.net/attachments/1027584701415833620/1032191239577153586/deliveboo-logo-invert-desktop.png"
+            alt="logo"
+          />
         </div>
 
         <!-- Colonna join us -->
-        <div class="col-3 mb-5">
+        <div class="col-3">
           <!-- Links -->
           <h5 class="text-uppercase font-weight-bold h4">join us</h5>
           <ul class="list-unstyled">
-            <li class="my-2">
+            <li class="mt-3">
               <a href="#!">Careers</a>
             </li>
-            <li class="my-2">
+            <li class="mt-2">
               <a href="#!">Restaurants</a>
             </li>
-            <li class="my-2">
+            <li class="mt-2">
               <a href="#!">Become a Courier</a>
             </li>
-            <li class="my-2">
+            <li class="mt-2">
               <a href="#!">About</a>
             </li>
           </ul>
         </div>
 
         <!-- Colonna Let us help you -->
-        <div class="col-3 mb-5">
+        <div class="col-3">
           <!-- Links -->
           <h5 class="text-uppercase font-weight-bold h4">let us help you</h5>
 
           <ul class="list-unstyled">
-            <li class="my-2">
+            <li class="mt-3">
               <a href="#!">Help Center</a>
             </li>
-            <li class="my-2">
+            <li class="mt-2">
               <a href="#!">Support</a>
             </li>
-            <li class="my-2">
+            <li class="mt-2">
               <a href="#!">Contacts</a>
             </li>
           </ul>
         </div>
 
         <!-- Colonna Follow us -->
-        <div class="col-3 mb-5">
+        <div class="col-3">
           <!-- Links 3 -->
           <h5 class="text-uppercase font-weight-bold h4">follow us</h5>
 
           <ul class="list-unstyled">
-            <li class="my-2">
+            <li class="mt-3">
               <a href="#!">Facebook</a>
             </li>
-            <li class="my-2">
+            <li class="mt-2">
               <a href="#!">Twitter</a>
             </li>
-            <li class="my-2">
+            <li class="mt-2">
               <a href="#!">Instagram</a>
             </li>
           </ul>
@@ -71,15 +72,17 @@
       <div class="col-12 divisor"></div>
 
       <!-- Copyright -->
-      <div class="copyright-container d-flex justify-content-between mt-4">
-        <div class="footer-copyright">
-        <p>© All right reserved Made &#129505; by Team 4 - Class #67</p>
-      </div>
-      <div class="footer-copyright-links">
-        <p>
-            <a class="mx-2" href="#!">Privacy Policy</a> | <a class="mx-2" href="#!">Terms & condition</a> | <a class="mx-2" href="#!">Cookies Policy</a>
-        </p>
-      </div>
+      <div class="copyright-container d-flex justify-content-between my-4">
+        <div class="footer-copyright mt-4">
+          <p>© All right reserved Made &#129505; by Team 4 - Class #67</p>
+        </div>
+        <div class="footer-copyright-links mt-4">
+          <p>
+            <a class="mx-2" href="#!">Privacy Policy</a> |
+            <a class="mx-2" href="#!">Terms & condition</a> |
+            <a class="mx-2" href="#!">Cookies Policy</a>
+          </p>
+        </div>
       </div>
     </div>
   </footer>
@@ -102,6 +105,7 @@ footer {
 
 a {
   color: $secondaryColor;
+  font-weight: 300;
 }
 
 li,
@@ -111,7 +115,7 @@ h5 {
 }
 
 .copyright-container {
-  p{
+  p {
     font-size: 0.7rem;
   }
 }
