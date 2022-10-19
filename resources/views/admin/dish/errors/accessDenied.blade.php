144e9bc150ccb705d965a4c627ec2533314f97be
vtnboolean <vnthuanx@gmail.com>
2022-10-19 19:23:38 +0200

    style: :ambulance: button style fixed

--- resources/views/admin/dish/errors/accessDenied.blade.php
+++ resources/views/admin/dish/errors/accessDenied.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="d-flex">
         <div class="container-fluid myBackground text-center align-self-center">
-            <img class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-4  error-image pt-3" src="{{ asset('images/hamburger.png') }}" alt="no pass">
+            <img class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 error-image pt-3" src="{{ asset('images/hamburger.png') }}" alt="no pass">
         </div>
     </div>
 
@@ -11,9 +11,9 @@
     <div class="text-error text-center">Sorry, but you are not allowed to enter this section.</div>
 
     <div class="">
-        <div class="mx-auto w-25">
-            <div class="my-btn my-btn-shadow mt-5 rounded-pill text-center">
-                <a class="nav-link px-0" href="/">Go Home</a>
+        <div class="mx-auto my-btn-errorpage">
+            <div class="my-btn my-btn my-btn-shadow mt-5 p-1 rounded-pill text-center">
+                <a class="nav-link px-0 white" href="/">Go Home</a>
             </div>
         </div>
     </div>
