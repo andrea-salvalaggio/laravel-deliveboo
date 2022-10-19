9acccf2830628574c79e321f486e90a81b0c2744
vtnboolean <vnthuanx@gmail.com>
2022-10-19 18:31:48 +0200

    fix: :ambulance: button Go home fixed

--- resources/views/admin/dish/errors/accessDenied.blade.php
+++ resources/views/admin/dish/errors/accessDenied.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
+    <div class="d-flex">
         <div class="container-fluid myBackground text-center align-self-center">
             <img class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-4  error-image pt-3" src="{{ asset('images/hamburger.png') }}" alt="no pass">
         </div>
     </div>
 
-    <h1 class="text-center">Page Not Found</h1>
-    <div class="text-error text-center">Sorry, but the page you are looking for doesn’t exist.</div>
-    <div class="text-error text-center">But there’s loads more to see!</div>
-    <div class="nav-item my-btn rounded-pill py-1 px-4 my-btn-shadow mt-2 mt-md-0 text-center ">
-        <a class="nav-link text-white px-0" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+    <h1 class="text-center">Access Denied</h1>
+    <div class="text-error text-center">Sorry, but you are not allowed to enter this section.</div>
+    <div class="text-center">
+        <div class="button btn btn-warning mt-5 rounded-pill px-5 mb-5 white">
+            <a class="nav-link px-0" href="/">Go Home</a>
+        </div>
     </div>
+
 
 @endsection
