9bf28414b8a9df99a2a76c8e92edd0e1249b51fc
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-19 14:47:37 +0200

    style: :art: Fix message container in show dish

--- resources/views/admin/dish/show.blade.php
+++ resources/views/admin/dish/show.blade.php
@@ -2,18 +2,20 @@
 
 @section('content')
     <div class="container-lg my-5">
-        @if (session('edited'))
+        <div class="col-lg-6 col-md-6 col-sm-10 mx-auto mb-4">
+            @if (session('edited'))
             <div class="alert alert-success" role="alert">
                 <span class="text-capitalize">{{ session('edited') }}</span>
             </div>
-        @endif
+            @endif
 
-        @if (session('created'))
-            <div class="alert alert-success" role="alert">
-                <span class="text-capitalize">{{ session('created') }}</span>
-            </div>
-        @endif
-
+            @if (session('created'))
+                <div class="alert alert-success" role="alert">
+                    <span class="text-capitalize">{{ session('created') }}</span>
+                </div>
+            @endif
+        </div>
+        
         <div class="card col-lg-6 col-md-6 col-sm-10 my-rounded my-shadow mx-auto border-0">
             {{-- @dd(asset('/' . $dish->dishPic)) --}}
 
