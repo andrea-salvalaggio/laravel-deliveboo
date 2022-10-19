1107cd80ddbfe2d1439dfc3be3202f74f97c520b
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-19 18:34:36 +0200

    style: :art: Add title to jumbotron

--- resources/js/pages/HomeGuest.vue
+++ resources/js/pages/HomeGuest.vue
@@ -1,16 +1,40 @@
 <template>
   <div>
     <!-- banner -->
-    <div class="container-jumbo d-none d-lg-block"></div>
+    <div class="container-jumbo d-none d-lg-block position-relative">
 
+      <!-- testo principale -->
+      <div class="container-lg">
+        <div class="row">
+          <div class="col-8">
+            <h1 class="text-white big-title">Restaurant <span>Food</span>, takeaway and groceries <span>Delivered.</span></h1>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- categorie -->
     <div class="container-fluid slider mt-3">
-      <div class="row flex-nowrap ">
-        <div class="col-6 col-lg-2 my-3" v-for="category in categories" :key="category.id">
-          <div class="mx-auto  my-rounded my-shadow order-card d-flex align-items-center justify-content-center text-capitalize"  :id="'categoria'+category.id"
-              @click="activeCard(category)" >
-              {{ category.name }}
+      <div class="row flex-nowrap">
+        <div
+          class="col-6 col-lg-2 my-3"
+          v-for="category in categories"
+          :key="category.id"
+        >
+          <div
+            class="
+              mx-auto
+              my-rounded my-shadow
+              order-card
+              d-flex
+              align-items-center
+              justify-content-center
+              text-capitalize
+            "
+            :id="'categoria' + category.id"
+            @click="activeCard(category)"
+          >
+            {{ category.name }}
           </div>
         </div>
       </div>
@@ -19,111 +43,131 @@
     <!-- Ristoranti -->
     <div class="container-lg pt-5">
       <div class="row flex-wrap">
-        <RestaurantCard v-for="restaurant in restaurants" :key="restaurant.id" :restaurant="restaurant" />
+        <RestaurantCard
+          v-for="restaurant in restaurants"
+          :key="restaurant.id"
+          :restaurant="restaurant"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import RestaurantCard from '../components/RestaurantCard.vue';
+import axios from "axios";
+import RestaurantCard from "../components/RestaurantCard.vue";
 
 export default {
-  components:{
-    RestaurantCard
-},
-  data: function(){
-    return{
+  components: {
+    RestaurantCard,
+  },
+  data: function () {
+    return {
       categories: [],
-      currentActive: '',
+      currentActive: "",
       isClicked: false,
       idCategory: null,
       restaurants: [],
-    }
+    };
   },
-  methods:{
+  methods: {
     //! funzione per effettuare la chiamata
-    ApiCallAllCategories(){
-      axios.get('/api/category')
-      .then((result)=>{
-        this.categories= result.data.results
-        console.log(this.categories)
-      })
-      .catch((error)=>{
-        console.error(error)
-      })
+    ApiCallAllCategories() {
+      axios
+        .get("/api/category")
+        .then((result) => {
+          this.categories = result.data.results;
+          console.log(this.categories);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    activeCard(category){
-      if( document.querySelector('.active-card') != null ){
-        document.querySelector('.active-card').classList.remove('active-card')
+    activeCard(category) {
+      if (document.querySelector(".active-card") != null) {
+        document.querySelector(".active-card").classList.remove("active-card");
       }
-      this.idCategory = category.id
-      this.currentActive= document.getElementById('categoria'+this.idCategory).classList.add('active-card')
+      this.idCategory = category.id;
+      this.currentActive = document
+        .getElementById("categoria" + this.idCategory)
+        .classList.add("active-card");
 
-      console.log(this.idCategory)
+      console.log(this.idCategory);
     },
 
-    getRestaurants(){
-            axios.get(`/api/restaurant`)
-            .then((response) => {
-                console.log(response.data.results.data);
-                this.restaurants = response.data.results.data;
-            }).catch((error) => {
-                console.error(error)
-            });
-        }
+    getRestaurants() {
+      axios
+        .get(`/api/restaurant`)
+        .then((response) => {
+          console.log(response.data.results.data);
+          this.restaurants = response.data.results.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
-  created(){
+  created() {
     this.ApiCallAllCategories();
-    this.getRestaurants()
-  }
-}
+    this.getRestaurants();
+  },
+};
 </script>
 
 <style scoped lang="scss">
- @import '../../sass/variables';
+@import "../../sass/variables";
 
-    .order-card{
-        width: 90%;
-        height: 100px;
-        font-family: 'Poppins', sans-serif;
-        font-weight: 600;
-        color: $secondaryColor;
-        cursor: pointer;
-    }
-    .active-card{
-        background-color: $primaryColor;
-        color: white;
-    }
-    .slider{
-      width: 100%;
-      height: 200px;
-      overflow-x: scroll;
-      &::-webkit-scrollbar {
-        height: 5px;
-        border-radius: 5px;
+.order-card {
+  width: 90%;
+  height: 100px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  color: $secondaryColor;
+  cursor: pointer;
+}
+.active-card {
+  background-color: $primaryColor;
+  color: white;
+}
+.slider {
+  width: 100%;
+  height: 200px;
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    height: 5px;
+    border-radius: 5px;
+  }
 
-      }
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
 
-    /* Track */
-        &::-webkit-scrollbar-track {
-          background: transparent;
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background: $primaryColor;
+    border-radius: 5px;
+  }
+}
+.container-jumbo {
+  width: 100%;
+  height: 700px;
+  background-image: url("../../images/jumbotron.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: bottom;
+}
 
-        }
+.big-title{
+  position: absolute;
+  top: 200px;
+  font-size: 3.5rem;
+  line-height: 4.8rem;
+  font-weight: 600;
+  
+  span{
+    color: $primaryColor;
+  }
+}
 
-    /* Handle */
-        &::-webkit-scrollbar-thumb {
-          background: $primaryColor;
-          border-radius: 5px;
-        }
-    }
-    .container-jumbo{
-      width: 100%;
-      height: 900px;
-      background-image: url('../../images/jumbotron.jpg');
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: center;
-    }
 </style>
