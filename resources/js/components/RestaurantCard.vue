1a3d47345b6f2271ac16b19669b731e2adacb0a0
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-10-19 10:21:01 +0200

    feat: :sparkles: Add restaurant card to home Guest

---
+++ resources/js/components/RestaurantCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-6 col-md-4 my-3">
+        <div class="card-container bg-white my-rounded my-shadow">
+            <div class="img-container">
+                <img class="my-rounded" :src="restaurant.restaurantPic" :alt="restaurant.name"></img>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data: function () {
+        return {
+
+        }
+    },
+
+    props: {
+        restaurant: {
+            type: Object,
+            require: true
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.card-container {
+    width: 95%;
+
+    .img-container {
+        height: 174px;
+        width: 100%;
+        img {
+            width: 100%;
+            height: 174px;
+            object-fit: cover;
+        }
+    }
+}
+</style>
