ba0231f6c4d802c0f375a0a03dcd5dfc322be255
Vegadrone <spenaantonino@gmail.com>
2022-10-25 08:40:05 +0100

    feat: :sparkles: add dishes to order show

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -9,24 +9,19 @@
                 <i class="fa-solid fa-angle-left pr-2"></i>Go Back
             </a>
         </div>
-        
+
         <div class="card col-lg-6 col-md-6 col-sm-10 mx-auto my-rounded my-shadow mx-auto border-0">
             <div class="card-body p-5">
                 <h2 class="font-weight-bold">Details:</h2>
                 <p class="card-title text-capitalize mt-5">Name: &nbsp;{{ $order->name . ' ' . $order->surname}}</p>
                 <p class="card-text">Address: &nbsp;{{ $order->address }}</p>
+                <p class="card-text">Dishes: &nbsp;
+                    @foreach ( $order->dishes as $dish)
+                        {{ $dish->name }} x{{ $dish->pivot->quantity }} <br>
+                    @endforeach</p>
                 <p class="card-text text-capitalize">Other info: &nbsp;{{ $order->description }}</p>
                 <hr class="my-4">
                 <p class="mb-0">Total Price: <span class="card-text font-weight-bolder h4 align-botton">&nbsp;€ {{ $order->price }}</span></p>
-                {{-- <div class="row">
-                    <div class="col-12 p-0 mt-5">
-                        <form action="{{ route('admin.order.destroy', $order->id) }}" method="POST" class="btn-delete">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn my-btn-danger w-100 rounded-pill text-white my-btn-shadow">Delete</button>
-                        </form>
-                    </div>
-                </div> --}}
             </div>
         </div>
     </div>
