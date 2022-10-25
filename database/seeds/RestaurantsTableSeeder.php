a190f3c1cb17d4945bce827db66d3c8c27e3969a
vtnboolean <vnthuanx@gmail.com>
2022-10-25 16:39:40 +0200

    perf: :zap: updated Restaurants seeder 1/3

--- database/seeds/RestaurantsTableSeeder.php
+++ database/seeds/RestaurantsTableSeeder.php
@@ -17,40 +17,40 @@ class RestaurantsTableSeeder extends Seeder
             [
                 'user_id' => 1,
                 'name'=> 'McDonald\'s',
-                'address'=> 'Via Roma 27',
+                'address'=> 'Via Giacomo Antonini, 28',
                 'open'=> '07:00',
-                'close'=>'00:00',
+                'close'=>'02:00',
                 'restaurantPic'=> 'https://s41230.pcdn.co/wp-content/uploads/2021/12/McDonalds-Net-Zero-restaurant-HEADER.jpg',
 
             ],
 
             [
                 'user_id' => 2,
-                'name'=> 'Da Michele',
-                'address'=> 'Via Napoli 12',
-                'open'=> '11:00',
+                'name'=> 'Domino\'s Pizza',
+                'address'=> 'Via Guecello Tempesta, 57',
+                'open'=> '06:00',
                 'close'=>'23:00',
-                'restaurantPic'=> 'https://media-cdn.tripadvisor.com/media/photo-s/0f/4c/de/1d/da-michele.jpg',
+                'restaurantPic'=> 'https://www.bakingbusiness.com/ext/resources/2020/3/DominosRestaurant_Lead.jpg?t=1585667973&width=1080',
 
             ],
 
             [
                 'user_id' => 3,
-                'name'=> 'Los Pollos Hermanos',
-                'address'=> 'Via Albuquerque 66',
-                'open'=> '00:00',
-                'close'=>'23:59',
-                'restaurantPic'=> 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/05/01/15/lospolloshermanos.jpg?width=1200',
+                'name'=> 'Qdoba',
+                'address'=> 'via Aldo Manuzio, 28',
+                'open'=> '10:30',
+                'close'=>'23:00',
+                'restaurantPic'=> 'https://cdn.vox-cdn.com/thumbor/zBEzySds-coOKCa7Ymm2H56gr6A=/0x0:3500x2336/1200x800/filters:focal(1470x888:2030x1448)/cdn.vox-cdn.com/uploads/chorus_image/image/58037473/L_Omaha-1-2.0.0.jpg',
 
             ],
 
             [
                 'user_id' => 4,
-                'name'=> 'Ginza',
-                'address'=> 'Via Porrettana 15',
-                'open'=> '11:30',
-                'close'=>'23:59',
-                'restaurantPic'=> 'https://upload.wikimedia.org/wikipedia/commons/5/5c/San-ai_Building_at_night.jpg',
+                'name'=> 'Tim Hortons',
+                'address'=> 'via Marmorata, 39',
+                'open'=> '05:0',
+                'close'=>'23:00',
+                'restaurantPic'=> 'https://www.worldcoffeeportal.com/getattachment/acccf7e4-eeb8-4705-a9f0-0798660d3e0f/2111258_1600-Exterior-1-(1).jpg.aspx?lang=en-GB&width=700&height=466',
 
             ],
 
