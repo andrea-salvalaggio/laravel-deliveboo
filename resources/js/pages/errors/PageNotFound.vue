4dc4a383226e92140a22fd7ca703287073d846cb
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-25 17:30:06 +0200

    fix: :art: Add image fot 404 page

--- resources/js/pages/errors/PageNotFound.vue
+++ resources/js/pages/errors/PageNotFound.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-        ERRORE
+    <div class="container-lg">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <img class="img-fluid" src="../../../images/404-page.png" alt="404 page | Return Home">
+            </div>
+            <h3>Whoops! The page you are looking for is not on the menu</h3>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    name: 'PageNotFound',
 }
 </script>
 
