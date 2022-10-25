b3ea3ef9012e81e8ae36454dcb1395a7e3e52892
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-25 17:45:26 +0200

    fix: :art: Finish 404 page

--- resources/js/pages/errors/PageNotFound.vue
+++ resources/js/pages/errors/PageNotFound.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="container-lg">
-        <div class="row justify-content-center">
+        <div class="row">
+            <!-- Immagine 404 -->
             <div class="col-12">
                 <img class="img-fluid" src="../../../images/404-page.png" alt="404 page | Return Home">
             </div>
-            <h3>Whoops! The page you are looking for is not on the menu</h3>
+
+            <div class="col-12 justify-content-center">
+                <h4 class="text-center">Whoops! The page you are looking for is not on the menu</h4>
+            </div>
+
+            <!-- Bottoni in desktop -->
+            <div class="nav-item my-btn rounded-pill my-btn-shadow text-center mx-auto px-4 py-1 mt-3">
+                <a href="/" class="nav-link text-white my-btn rounded-pill my-btn-shadow px-0"><i class="fa-solid fa-house mr-2"></i>Back Home</a>
+            </div>
         </div>
     </div>
 </template>
