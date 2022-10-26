57dc66d753653c40d58ae54991f62b45f2d80563
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-10-26 15:46:23 +0200

    fix: :sparkles: Fix head padding to login and registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container head-padding">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card border-0 my-rounded my-shadow">
